5f3e9e1f4fba637b2f27e3364545602a9ca7f564
khargy <146685592+BenjarBulacon@users.noreply.github.com>
2024-07-15 16:38:56 -0700

    update colleges benjar

---
+++ college_of_engineering.php
@@ -0,0 +1,98 @@
+<link rel="stylesheet" href="styles/college_of_engineering.css"/>
+
+
+<body>
+    <div>
+        <?php include "colleges_topNav.php"; ?>
+
+        <div class="cet-bg-container">
+            <image src="assets/img/breadcumb/CETNEWBKG.png" class="cet-bg"></image>
+        </div>
+        <div class="cet-subNav-container">
+            <ul>
+                <li><a href="#" class="cet-home">COLLEGE OF ENGINEERING AND TECHNOLOGY</a></li>
+                <li class="li"><a href="#">ITE</a></li>
+                <li class="li"><a href="#"> CE</a></li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="logo-and-mv-container">
+        <div class="logo-container">
+            <image src="_assets/COLLEGE_LOGOS/CET.png" class="logo"></image>
+        </div>
+
+        <div class="mission-vission-container">
+
+            <div class="mission-container">
+                <div class="mission-text">Mission</div>
+                <p>
+                    Our mission is to offer a transformative educational program that develops critical
+                    thinking, creativity, and technical proficiency in engineering and technology. We want to build a
+                    diverse community of students and professors committed to solving complex problems and changing the
+                    world via cutting-edge research, industrial partnerships, and community involvement.
+                </p>
+            </div>
+
+            <div class="vission-container">
+                <div class="vission-text">Vission</div>
+                <p>
+                    To be recognized as an institution of excellence in engineering and technology
+                    education, research, and innovation that equips students to become ethical and skilled professionals
+                    who contribute to society and promote sustainable development.
+                </p>
+            </div>
+
+            <div class="goals-container">
+                <div class="goals-text">Goals</div>
+                <ul>
+                    <li>Academic Excellence </li>
+                    <li>Research and Innovation </li>
+                    <li>Student Success </li>
+                    <li>Global Engagement </li>
+                    <li>Community Outreach</li>
+                </ul>
+
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+
+
+
+    <div class="register-container">
+        <div class="link-container">
+            <h1>Learn From Our Platform</h1>
+            <h1>That Takes You To The Next level</h1>
+            <a href="http://system.rmmc.edu.ph/rmmcweb/collegeenroll">Register Now</a>
+        </div>
+       
+        
+        <div>
+            <image src="assets/img/normal/cta_1_1.png"></image>
+        </div>
+        <div>
+            <image src="assets/img/shape/dot_shape_1.png" class="dots-design"></image>
+        </div>
+      
+        
+    </div>
+
+
+
+
+
+
+
+
+
+    
+    
+
+</body>
+

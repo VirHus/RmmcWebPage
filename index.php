089253a6bfd26350abe4388bc72674eabc6864b7
kissanima <kissanima@gmail.com>
2024-07-15 13:17:05 +0800

    index

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/index.css">
-    <title>RMMC - Home</title>
+<?php include 'header.php' ?>
 
-</head>
-<body>
-    
-<dev class="hero-img1">
-<img alt="hero" src="assets/img/hero/hero_img_1_1_newnew.png">
-<div class="text">Your Text Here</div>
-</dev>
+
+<div class="as-hero-wrapper hero-1">
+  <div class="as-hero-bg background-image" data-opacity="7" data-overlay="overlay1" style="background-image: url(_assets/img/hero/hero_bg_1_1.html);"></div>
+
+  <div class="container z-index-common">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="hero-style1"><img alt="Rmmc" src="assets/img/pacu.png" style="width:120px;height:115px"> <img alt="Rmmc" src="assets/img/soco.png" style="width:120px;height:115px">
+          <h1 class="hero-title" style="font-size:25px">PACUCOA ACCREDITED SCHOOL &amp;<br>
+            ISO 9001:2015 CERTIFIED</h1>
+          <br>
+          <a class="as-btn style3" href="course.php">Enroll Now</a>
+        </div>
+      </div>
+
+      <div class="col-xl-6">
+        <div class="hero-img1"><img alt="hero" src="assets/img/hero/hero_img_1_1_newnew.png"></div>
+      </div>
+    </div>
+  </div>
+
+  
+  <div class="hero-shape shape1"><img alt="shape" src="assets/img/hero/shape_1_1.png"></div>
+
+  <div class="hero-shape shape2"><img alt="shape" src="assets/img/hero/shape_1_2.png"></div>
+
+  <div class="hero-shape shape3"></div>
+
+  <div class="ripple-shape"></div>
+</div>
 
 </body>
 </html>

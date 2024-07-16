236fac2aa6e3c9d081a9626f5d18918729119cdc
Kirt_12 <roxaskirt21@gmail.com>
2024-07-16 15:14:59 +0800

    making footer section

--- footer.php
+++ footer.php
@@ -1,117 +1,106 @@
-<?php include 'header.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styles/footer.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+
+    
+</head>
 <body>
-<footer class="footer-wrapper footer-layout1 shape-mockup-wrap" style="padding-top: 257px;">
-    <div class="widget-area">
-      <div class="container">
-        <div class="footer-row">
-          <div class="footer-col">
-            <div class="widget footer-widget">
-              <div class="as-widget-about">
-                <div class="about-logo">
-                  <a href="index.php"><img alt="Rmmc" src="assets/img/white-logo.png"></a>
-                </div>
-                <h4 class="footer-info-title">Follow Us On:</h4>
-                <div class="as-social">
-                  <a href="https://www.facebook.com/rmmcgensanofficial" target="_blank"><i class="fab fa-facebook-f"></i></a>
-                  <a href="https://www.twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a>
-                  <a href="https://ph.linkedin.com/in/rmmc-gensan-31b2a51b0" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-                  <a href="https://www.instagram.com/rmmcsmcc/" target="_blank"><i class="fab fa-instagram"></i></a>
-                  <a href="https://www.youtube.com/@rmmcgensan-marketingandpro5185" target="_blank"><i class="fab fa-youtube"></i></a>
-                </div>
-                <img src="assets/img/iso.png" alt="Rmmc" class="iso-img" />
-                <span class="iso-text">ISO 9001:2015 CERTIFIED</span>
-              </div>
-            </div>
-          </div>
+    <footer class = "footer_main" style ="padding-top: 257px">
+        <div class="footer_main__footer_area">
+         <div class="footer_main__footer_area__container">
+            <div class="footer_main__footer_area__container__row_footer">
 
-          <div class="footer-col">
-            <div class="widget widget_nav_menu footer-widget">
-              <h3 class="widget_title">Quick link</h3>
-              <div class="menu-all-pages-container">
-                <ul class="menu">
-                  <li><a href="team.php">Life Coach</a></li>
-                  <li><a href="team.php">Business Coach</a></li>
-                  <li><a href="team.php">Health Coach</a></li>
-                  <li><a href="UnderCon.php" target="_blank">Development</a></li>
-                  <li><a href="UnderCon.php" target="_blank">Web Design</a></li>
-                  <li><a href="UnderCon.php" target="_blank">SEO Optimize</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
 
-          <div class="footer-col">
-            <div class="widget widget_nav_menu footer-widget">
-              <h3 class="widget_title">Resources</h3>
-              <div class="menu-all-pages-container">
-                <ul class="menu">
-                  <li><a href="UnderCon.php" target="_blank">Community</a></li>
-                  <li><a href="contact.php">Support</a></li>
-                  <li><a href="UnderCon.php" target="_blank">Video Guides</a></li>
-                  <li><a href="about.php">Documentation</a></li>
-                  <li><a href="contact.php">Security</a></li>
-                  <li><a href="UnderCon.php" target="_blank">Template</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div class="footer-col">
-            <div class="widget footer-widget">
-              <h3 class="widget_title">Get in touch!</h3>
-              <form class="newsletter-widget">
-                <div class="form-group">
-                  <input class="form-control" placeholder="Enter Email" required="" type="email">
+                <div class="footer_main__footer_area__container__row_footer__col_footer">
+                        <div class="footer_main__footer_area__container__row_footer__col_footer__object_about">
+                            <div class="object_logo">
+                                <a href="#"><img alt="Rmmc" src="assets/img/white-logo.png"></a>   
+                            </div>
+                        <h4 class="footer_info_title">Follow Us On:</h4>
+                        <div class="social">
+                            <a href="https://www.facebook.com/rmmcgensanofficial" target ="_block"><i class="fab fa-facebook-f"></i></a>
+                            <a href="https://www.twitter.com/" target ="_block"><i class="fab fa-twitter"></i></a>
+                            <a href="https://ph.linkedin.com/in/rmmc-gensan-31b2a51b0" target ="_block"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="https://www.instagram.com/rmmcsmcc/"target ="_block"><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.youtube.com/@rmmcgensan-marketingandpro5185"target ="_block"><i class="fab fa-youtube"></i></a>
+                        </div>
+                        <img src="assets/img/iso.png" alt="Rmmc" class="iso_img"/>
+                        <span class="iso_text">ISO 9001:2015 CERTIFIED</span>
+                        </div>
                 </div>
-                <button class="as-btn style3 shadow-none" type="submit">Subscribe</button>
-              </form>
+
+                <div class="footer_main__footer_area__container__row_footer__col_footer">
+                    <div class="object nav footer_object">
+                        <h3 class="object_title">Quck Link</h3>
+                        <div class="all_pages_container">
+                            <ul class="all_pages_menu">
+                                <li><a href="#">Life Coach</a></li>
+                                <li><a href="#">Business Coach</a></li>
+                                <li><a href="#">Health Coach</a></li>
+                                <li><a href="#">Development</a></li>
+                                <li><a href="#">Web Design</a></li>
+                                <li><a href="#">SEO Optimize</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col_footer">
+                    <div class="object nav footer_object">
+                        <h3 class="object_title">Resources</h3>
+                        <div class="all_pages_container">
+                            <ul class="all_pages_menu">
+                                <li><a href="#">Community</a></li>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="#">Video Guides</a></li>
+                                <li><a href="#">Documentation</a></li>
+                                <li><a href="#">Security</a></li>
+                                <li><a href="#">Template</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col_footer">
+                    <div class="object footer_object form">
+                        <h3 class="object_title">Get in touch!</h3>
+                        <form class="newsletter_object">
+                            <div class="form_group">
+                                <input type = "email" class="form_control" placeholder = "Enter Email" required = "">
+                                <i class = "fal fa-envelope"></i>
+                            </div>
+                            <button class="subs_btn btn_style" type ="submit">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </div>    
+         </div>
+       </div>
+
+       <div class="footer_main__container">
+        <div class="copyright_wrap">
+            <div class="row_footer">
+                <div class="col_footer">
+                    <p class="copyright_txt">Copyright 2024 <a href="#">Custard Apple</a>. All Rights Reserved</p>
+                </div>
+                <div class="col_footer text_end">
+                    <div class="footer_links">
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">Terms of Use</a>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="copyright-wrap">
-        <div class="footer-row">
-          <div class="footer-col">
-            <p class="copyright-text">Copyright 2023 <a href="#">Custard Apple</a>. All Rights Reserved.</p>
-          </div>
-          <div class="footer-col text-end">
-            <div class="footer-links">
-              <ul>
-                <li><a href="UnderCon.php" target="_blank">Privacy Policy</a></li>
-                <li><a href="UnderCon.php" target="_blank">Terms of Use</a></li>
-              </ul>
-            </div>
-          </div>
+       </div>
+       <div class="bckgrnd_img">
+        <img src="assets/img/shape/footer_shape_3.png" alt="background">
         </div>
-      </div>
-    </div>
-
-    <div class="shape-mockup jump d-none d-xl-block" style="top: 24%; left: 4%;">
-      <img alt="shapes" src="assets/img/shape/footer_shape_1.png">
-    </div>
-    <div class="shape-mockup jump-reverse d-none d-xl-block" style="right: 4%; bottom: 20%;">
-      <img alt="shapes" src="assets/img/shape/footer_shape_2.png">
-    </div>
-    <div class="shape-mockup" style="top: 0px; right: 0px;">
-      <img alt="shapes" src="assets/img/shape/footer_shape_3.png">
-    </div>
-  </footer>
-  <a href="#" class="scrollToTop scroll-btn"><i class="far fa-arrow-up"></i></a>
-  <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
-  <script src="assets/js/app.min.js"></script>
-  <script src="assets/js/main.js"></script>
-</body>
-</html>
-
-    <a href="#" class="scrollToTop scroll-btn"><i class="far fa-arrow-up"></i></a>
-    <script src="RmmcWebPage/assets/js/vendor/jquery-3.6.0.min.js"></script>
-    <script src="RmmcWebPage/assets/js/app.min.js"></script>
-    <script src="RmmcWebPage/assets/js/main.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css"> 
-    <link rel="stylesheet" href="footer.css"> 
+    </footer>
+        
 </body>
 </html>

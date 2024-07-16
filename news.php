5f363b96ed375eccd6b08e5d531470e83093a381
23Norm <normanjbernasor@gmail.com>
2024-07-16 10:49:42 +0800

    added some changes

--- news.php
+++ news.php
@@ -1,11 +1,10 @@
 
-<link defer rel="stylesheet" href="styles/news.css">
-
+<link rel="stylesheet" href="styles/news.css">
 <body>
 
   <div class="breadcrumb-card"  style="background-image: url(_assets/img/breadcumb/breadcumb-bg.html);">
     <div class="breadcrumb-card__box">
-        <h1 class="breadcrumb-card__box__title">Our News</h1>
+        <h1 class="breadcrumb-card__box__title">OUR NEWS</h1>
         <ul class="breadcrumb-card__box__menu">
           <a class="breadcrumb-card__box__menu__item__link" href="index.php">Home</a></li>
         </ul>
@@ -26,11 +25,11 @@
       </div>
 
       <div class="news-card__item__element__date-and-time">
-        <div class="news-card__item__date-and-time__view">
+        <div class="news-card__item__element__date-and-time__view">
           <h1>VIEW</h1>
         </div>
         
-        <div class="news-card__item__date-and-time__display">
+        <div class="news-card__item__element__date-and-time__display">
           <h3>May 7, 2024</h3>
           <h3>12:58 AM</h3>
         </div>
@@ -41,10 +40,122 @@
     <img id="news-card__item__img" src="norman/img/cardo.jpeg" alt="">
   </div>
 
-  <!-- <div class="news-card__item">
+  <div class="news-card__item">
+    <div class="news-card__item__element">
+      <div class="news-card__item__element__author">
+        <img id="news-card__item__element__author__img" src="norman/img/jason_bourne.jpeg" alt="">
+        <h4>Author name</h4>
+      </div>
+      
+      <div class="news-card__item__element__description">
+          <h2>Jason Bourne</h2>
+          <h4>Ass-sasin</h4>
+      </div>
+
+      <div class="news-card__item__element__date-and-time">
+        <div class="news-card__item__element__date-and-time__view">
+          <h1>VIEW</h1>
+        </div>
+        
+        <div class="news-card__item__element__  date-and-time__display">
+          <h3>May 7, 2024</h3>
+          <h3>12:58 AM</h3>
+        </div>
+
+      </div>
+    </div>
+
+    <img id="news-card__item__img" src="norman/img/jason_bourne.jpeg" alt="">
+  </div>
+
+  <div class="news-card__item">
+    <div class="news-card__item__element">
+      <div class="news-card__item__element__author">
+        <img id="news-card__item__element__author__img" src="norman/img/john_wick.jpeg" alt="">
+        <h4>Author name</h4>
+      </div>
+      
+      <div class="news-card__item__element__description">
+          <h2>Johnny bravo wick</h2>
+          <h4>skilled killer</h4>
+      </div>
+
+      <div class="news-card__item__element__date-and-time">
+        <div class="news-card__item__element__date-and-time__view">
+          <h1>VIEW</h1>
+        </div>
+        
+        <div class="news-card__item__element__date-and-time__display">
+          <h3>May 7, 2024</h3>
+          <h3>12:58 AM</h3>
+        </div>
+
+      </div>
+    </div>
+
+    <img id="news-card__item__img" src="norman/img/john_wick.jpeg" alt="">
+  </div>
+
+  <div class="news-card__item">
+    <div class="news-card__item__element">
+      <div class="news-card__item__element__author">
+        <img id="news-card__item__element__author__img" src="norman/img/jason_bourne.jpeg" alt="">
+        <h4>Author name</h4>
+      </div>
+      
+      <div class="news-card__item__element__description">
+          <h2>Born again si jason born</h2>
+          <h4>Ni agto ni ug pinas</h4>
+      </div>
+
+      <div class="news-card__item__element__date-and-time">
+        <div class="news-card__item__element__date-and-time__view">
+          <h1>VIEW</h1>
+        </div>
+        
+        <div class="news-card__item__element__date-and-time__display">
+          <h3>May 7, 2024</h3>
+          <h3>12:58 AM</h3>
+        </div>
+
+      </div>
+    </div>
+
+    <img id="news-card__item__img" src="norman/img/jason_bourne.jpeg" alt="">
+  </div>
+
+  <div class="news-card__item">
     <div class="news-card__item__element">
       <div class="news-card__item__element__author">
         <img id="news-card__item__element__author__img" src="norman/img/cardo.jpeg" alt="">
+        <h4>Author name</h4>
+      </div>
+      
+      <div class="news-card__item__element__description">
+          <h2>Gi sugo ko ni cardo</h2>
+          <h4>Palit daw ko ug asin</h4>
+      </div>
+
+      <div class="news-card__item__element__date-and-time">
+        <div class="news-card__item__element__date-and-time__view">
+          <h1>VIEW</h1>
+        </div>
+        
+        <div class="news-card__item__element__date-and-time__display">
+          <h3>May 7, 2024</h3>
+          <h3>12:58 AM</h3>
+        </div>
+
+      </div>
+    </div>
+
+    <img id="news-card__item__img" src="norman/img/cardo.jpeg" alt="">
+  </div>
+
+  <div class="news-card__item">
+    <div class="news-card__item__element">
+      <div class="news-card__item__element__author">
+        <img id="news-card__item__element__author__img" src="norman/img/john_wick.jpeg" alt="">
         <h4>Author name</h4>
       </div>
       
@@ -54,11 +165,11 @@
       </div>
 
       <div class="news-card__item__element__date-and-time">
-        <div class="news-card__item__date-and-time__view">
+        <div class="news-card__item__element__date-and-time__view">
           <h1>VIEW</h1>
         </div>
         
-        <div class="news-card__item__date-and-time__display">
+        <div class="news-card__item__element__date-and-time__display">
           <h3>May 7, 2024</h3>
           <h3>12:58 AM</h3>
         </div>
@@ -66,7 +177,7 @@
       </div>
     </div>
 
-    <img id="news-card__item__img" src="norman/img/cardo.jpeg" alt="">
+    <img id="news-card__item__img" src="norman/img/john_wick.jpeg" alt="">
   </div>
 
 
@@ -83,11 +194,11 @@
       </div>
 
       <div class="news-card__item__element__date-and-time">
-        <div class="news-card__item__date-and-time__view">
+        <div class="news-card__item__element__date-and-time__view">
           <h1>VIEW</h1>
         </div>
         
-        <div class="news-card__item__date-and-time__display">
+        <div class="news-card__item__element__date-and-time__display">
           <h3>May 7, 2024</h3>
           <h3>12:58 AM</h3>
         </div>
@@ -96,6 +207,34 @@
     </div>
 
     <img id="news-card__item__img" src="norman/img/cardo.jpeg" alt="">
-  </div> -->
+  </div>
+
+  <div class="news-card__item">
+    <div class="news-card__item__element">
+      <div class="news-card__item__element__author">
+        <img id="news-card__item__element__author__img" src="norman/img/jason_bourne.jpeg" alt="">
+        <h4>Author name</h4>
+      </div>
+      
+      <div class="news-card__item__element__description">
+          <h2>Pasigarbo ni cardo</h2>
+          <h4>Isang magiting na police na lumalaban para sa hustisya</h4>
+      </div>
+
+      <div class="news-card__item__element__date-and-time">
+        <div class="news-card__item__element__date-and-time__view">
+          <h1>VIEW</h1>
+        </div>
+        
+        <div class="news-card__item__element__date-and-time__display">
+          <h3>May 7, 2024</h3>
+          <h3>12:58 AM</h3>
+        </div>
+
+      </div>
+    </div>
+
+    <img id="news-card__item__img" src="norman/img/jason_bourne.jpeg" alt="">
+  </div>
 </body>
 </html>

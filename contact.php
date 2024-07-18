c1698f14da75d5ac62daaee5620e67edd8f0a9be
VirHus <lesterjohn623@gmail.com>
2024-07-18 08:48:06 +0800

    updated

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+$page = 'contact';
+require 'header.php';
+?>
+
+
+<body style="overflow-x:hidden">
+    <?php include "nav_bar.php" ?>
+    
+    <div class="breadcrumb-card" style="background-image: url(_assets/img/breadcumb/breadcumb-bg.html);">
+        <div class="breadcrumb-card__box">
+            <h1 class="breadcrumb-card__box__title">OUR NEWS</h1>
+            <ul class="breadcrumb-card__box__menu">
+                <a class="breadcrumb-card__box__menu__item__link" href="index.php">Home</a></li>
+            </ul>
+        </div>
+    </div>
+
+    
+
+
+</body>
+<?php include 'footer.php' ?>
+
+</html>

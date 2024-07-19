c69fba17ba2eb6e31943d3fa6adf6b54cd1daa9f
VirHus <lesterjohn623@gmail.com>
2024-07-19 14:10:10 +0800

    header

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,5 @@
-// const menuItemsButton= document.querySelector('.menu_items_button');
-// const menuHeaderMainMenu = document.querySelector('.menu_header_main_menu');
+// const menuItemsButton= document.querySelector('.menu_header__left__button');
+// const menuHeaderMainMenu = document.querySelector('.menu_header__right');
 
 // menuItemsButton.onclick = () => menuHeaderMainMenu.classList.toggle('active');
 
@@ -8,10 +8,10 @@
 
 // colleges_List.onclick = () => colleges_cont.classList.toggle('active')
 
-// const menuItemsParent = document.querySelector('.menu_header_main_menu')
+// const menuItemsParent = document.querySelector('.menu_header__right')
 
 // menuItemsParent.onclick = event=> {
-//     const sublistCollection = [...document.querySelectorAll('.menu_header_main_menu_has_children__sublist')];
+//     const sublistCollection = [...document.querySelectorAll('.menu_header__right__item__list')];
 // ``
 
 //     if(sublistCollection && sublistCollection.length > 0){
@@ -20,10 +20,10 @@
 //     /**
 //      * @type {HTMLElement}
 //      */
-//     const isMenuItemTitle = event.target.classList.contains("menu_header_main_menu_has_children");
+//     const isMenuItemTitle = event.target.classList.contains("menu_header__right__item");
 
 //     if(isMenuItemTitle){
-//         const sublist = event.target.querySelector('.menu_header_main_menu_has_children__sublist');
+//         const sublist = event.target.querySelector('.menu_header__right__item__list');
 //         if(!sublist){
 //             return;
 //         }
@@ -34,62 +34,13 @@
 
 // //for colleges js function adding toggle
 
-const menuItemsButton = document.querySelector('.menu_items_button');
-const menuHeaderMainMenu = document.querySelector('.menu_header_main_menu');
+const menuItemsButton = document.querySelector('.menu_header__left__button');
+const menuHeaderMainMenu = document.querySelector('.menu_header__right');
 
 menuItemsButton.onclick = () => menuHeaderMainMenu.classList.toggle('active');
 
-const menuItemsParent = document.querySelector('.menu_header_main_menu');
+const menuItemsParent = document.querySelector('.menu_header__right');
 
 menuItemsParent.onclick = (event) => {
-  const sublistCollection = [
-    ...document.querySelectorAll(
-      '.menu_header_main_menu_has_children__sublist'
-    ),
-  ];
-
-  const innerSublistCollection = Array.from(
-    document.querySelectorAll(
-      '.menu_header_main_menu_has_children__sublist__sublist'
-    )
-  );
-
-  // close all active inner sublist
-  if (innerSublistCollection && innerSublistCollection.length > 0) {
-    innerSublistCollection.forEach((sublist) =>
-      sublist.lastElementChild.classList.remove('active')
-    );
-  }
-
-  // open specific inner sublist
-  const clickedOnInnerSublist = event.target.classList.contains(
-    'menu_header_main_menu_has_children__sublist__sublist'
-  );
-
-  if (clickedOnInnerSublist) {
-    event.target.lastElementChild.classList.add('active');
-    return;
-  }
-
-  if (sublistCollection && sublistCollection.length > 0) {
-    sublistCollection.forEach((sublist) => sublist.classList.remove('active'));
-  }
-  /**
-   * @type {HTMLElement}
-   */
-  const isMenuItemTitle = event.target.classList.contains(
-    'menu_header_main_menu_has_children'
-  );
-
-  if (isMenuItemTitle) {
-    const sublist = event.target.querySelector(
-      '.menu_header_main_menu_has_children__sublist'
-    );
-
-    if (!sublist) {
-      return;
-    }
-
-    sublist.classList.add('active');
-  }
+  // pass
 };

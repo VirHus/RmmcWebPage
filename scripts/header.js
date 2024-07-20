45800c15a21126df6701250940539d3b1b7bc9c9
AceBurgundy <samadriansabalo99@gmail.com>
2024-07-20 09:50:55 +0800

    Made the code readable.

--- scripts/header.js
+++ scripts/header.js
@@ -7,18 +7,23 @@ const menuItemsParent = document.querySelector('.menu_header__right');
 
 // This code is a simple concept of, if a ul exist inside an li, set it active.
 menuItemsParent.onclick = (event) => {
-  // Clears all open sublist
   const ulCollection = [...menuItemsParent.querySelectorAll('ul')];
 
-  if (ulCollection.length > 0) {
-    ulCollection.forEach((ulElement) => {
-      if (!ulElement.contains(event.target)) {
-        ulElement.classList.remove('active');
-      }
-    });
+  if (ulCollection.length <= 0) {
+    return;
   }
 
-  // Code to open sublists
+  // Clears all open sublist
+  ulCollection.forEach((ulElement) => {
+    //  Except if the target is a nested sublist, it prevents its active parent to be closed
+    if (ulElement.contains(event.target)) {
+      return;
+    }
+
+    ulElement.classList.remove('active');
+  });
+
+  // Opens the targeted sublist
   if (event.target.tagName === 'LI') {
     const listElements = [...event.target.children].filter((child) => child.tagName === 'UL');
     const sublist = listElements.length > 0 ? listElements[0] : null;
